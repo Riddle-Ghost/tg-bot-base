a3ba3fda74fba72bb1612f62f7b363a92360d913
riddle <runprogr@gmail.com>
2026-02-16 20:54:59 +0300

    init

--- src/TgBotBase/Db/MigrationService.php
+++ src/TgBotBase/Db/MigrationService.php
@@ -2,25 +2,26 @@
 
 namespace Riddle\TgBotBase\Db;
 
+use Riddle\TgBotBase\Ai\Db\AiContextMigration;
+use Riddle\TgBotBase\Log\Db\LogMigration;
+use Riddle\TgBotBase\Db\SeedMigration;
+use Riddle\TgBotBase\User\Db\UserMigration;
+
 require_once __DIR__ . '/rb-sqlite.php';
 
 class MigrationService
 {
-    public const string DB_USERS = 'users';
-    public const string DB_TECH = 'tech';
-    public const string DB_AI = 'ai';
-
     public function __construct(
         public readonly DbConfig $config,
     ) {}
 
     public function migrateAll(): void
     {
-        // \R::setup(self::getDsn(self::DB_USERS));
-        $this->migrate(self::users());
-        $this->migrate(self::aiContexts());
-        $this->migrate(self::logs());
-        $this->migrate(self::seeds());
+        \R::setup($this->getDsn(UserMigration::DB_NAME));
+        $this->migrate(new UserMigration());
+        $this->migrate(new AiContextMigration());
+        $this->migrate(new LogMigration());
+        $this->migrate(new SeedMigration());
         
         foreach ($this->config->migrations as $migration) {
             $this->migrate($migration);
@@ -48,81 +49,5 @@ class MigrationService
     public function getPath(string $dbName): string
     {
         return $this->config->dbDir . '/' . $dbName . '.sqlite';
-    }
-
-    private function users(): MigrationDto
-    {
-        $createTableSql = "CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tg_id BIGINT UNIQUE,
-            username VARCHAR(255),
-            is_premium BOOLEAN DEFAULT 0,
-            is_blocked BOOLEAN DEFAULT 0,
-            settings JSON DEFAULT '{}',
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        )";
-
-        return new MigrationDto(
-            dbName: self::DB_USERS,
-            createTableSql: $createTableSql,
-            indexSql: [
-                "CREATE UNIQUE INDEX IF NOT EXISTS idx_tg_id ON users (tg_id)",
-            ]
-        );
-    }
-
-    private function aiContexts(): MigrationDto
-    {
-        $createTableSql = "CREATE TABLE IF NOT EXISTS ai_contexts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tg_id BIGINT UNIQUE,
-            context TEXT,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        )";
-
-        return new MigrationDto(
-            dbName: self::DB_AI,
-            createTableSql: $createTableSql,
-            indexSql: [
-                "CREATE UNIQUE INDEX IF NOT EXISTS idx_tg_id ON ai_contexts (tg_id)",
-            ]
-        );
-    }
-
-    private function logs(): MigrationDto
-    {
-        $createTableSql = "CREATE TABLE IF NOT EXISTS logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            type VARCHAR(20) UNIQUE,
-            user_id INTEGER,
-            text TEXT,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        )";
-
-        return new MigrationDto(
-            dbName: self::DB_TECH,
-            createTableSql: $createTableSql,
-            indexSql: [
-                "CREATE UNIQUE INDEX IF NOT EXISTS idx_type ON logs (type)",
-                "CREATE  INDEX IF NOT EXISTS idx_user_id ON logs (user_id)",
-            ]
-        );
-    }
-
-    private function seeds(): MigrationDto
-    {
-        $createTableSql = "CREATE TABLE IF NOT EXISTS seeds (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            file VARCHAR(255) UNIQUE,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        )";
-
-        return new MigrationDto(
-            dbName: self::DB_TECH,
-            createTableSql: $createTableSql,
-            indexSql: [],
-        );
     }
 }
